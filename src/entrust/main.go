1f27c6f9ad8a2984df479f3a8f05d72794273178
netgene <netgene@hotmail.com>
2016-05-31 17:26:33 +0800

    etr

---
+++ src/entrust/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	logger "github.com/donnie4w/go-logger"
+	"config"
+	//"fmt"
+)
+
+func main() {
+	//defer logger.Flush()
+	logger.SetRollingFile("log", "entrust.log", 10, 5, logger.KB)
+	logger.SetRollingDaily("log", "entrust.log")
+	logger.SetLevel(logger.INFO)
+	//fmt.Println("Hello, GO!")
+	logger.Info("hello go!")
+	config.LoadConfig()
+}
